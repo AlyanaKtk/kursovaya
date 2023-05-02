9f53a515ab655e2ac04f1af53402d11adc393e08
Elena <catswariors2003@gmail.com>
2023-05-02 22:58:48 +0300

    second commit

--- src/main/java/com/example/krs_test/controllers/BookController.java
+++ src/main/java/com/example/krs_test/controllers/BookController.java
@@ -3,14 +3,15 @@ package com.example.krs_test.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class BookController
 {
-    @RequestMapping("/")
+    @RequestMapping ("/")
     public String helloMessage(Model model) {
-        return "books";
+        return "books.html";
     }
 }
